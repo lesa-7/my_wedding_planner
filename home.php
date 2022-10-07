e5a62723269d56be7ee2c83a988c77bf634b5537
Lesa RAFFIN-GAULTIER <lesa.raffin.gaultier@gmail.com>
2022-10-07 20:49:31 +0200

    footer section added

--- home.php
+++ home.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
+
 <!-- custom css file link -->
     <link rel="stylesheet" href="css/style.css">
     
-    <!-- font awesome cdn link -->
+<!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
     
@@ -16,33 +17,20 @@
     
 
 </head>
-<body>
 
+<body>
 
 <div class="container">
 
 <?php @include 'header.php'; ?>
 
+<?php @include 'footer.php'; ?>
+
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!-- custom js file link  -->
-
 <script src="js/script.js"></script>
-
-    
+  
 </body>
+
 </html>
