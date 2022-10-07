6ee71c20838550e1870d612ce61b9d39dda5b504
Lesa RAFFIN-GAULTIER <lesa.raffin.gaultier@gmail.com>
2022-10-07 22:48:27 +0200

    home section added

--- home.php
+++ home.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
 
+<!-- font awesome cdn link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+<!-- swiper css link -->
+<link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
 <!-- custom css file link -->
     <link rel="stylesheet" href="css/style.css">
     
-<!-- font awesome cdn link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
-    
-
-    
+ 
 
 </head>
 
@@ -24,9 +26,55 @@
 
 <?php @include 'header.php'; ?>
 
+<section class="home">
+    <div class="swiper home-slider">
+    <div class="swiper-wrapper">
+
+        <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+            <div class="content">
+                <h3>Plan your wedding!</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Minus tempore, commodi nisi aperiam illum, cumque rerum tempora asperiores, 
+                    tenetur dolorum.</p>
+                <a href="about.php" class="btn">discover more</a>
+            </div>
+        </div>
+
+
+        <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+            <div class="content">
+                <h3>Plan your wedding!</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Minus tempore, commodi nisi aperiam illum, cumque rerum tempora asperiores, 
+                    tenetur dolorum.</p>
+                <a href="about.php" class="btn">discover more</a>
+            </div>
+        </div>
+
+        <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+            <div class="content">
+                <h3>Plan your wedding!</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Minus tempore, commodi nisi aperiam illum, cumque rerum tempora asperiores, 
+                tenetur dolorum.</p>
+                <a href="about.php" class="btn">discover more</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="swiper-pagination"></div>
+
+    </div>
+
+</section>
+
 <?php @include 'footer.php'; ?>
 
 </div>
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
