1587fb73d6aa7f88f057b421b199cc875523a81b
Lesa RAFFIN-GAULTIER <lesa.raffin.gaultier@gmail.com>
2022-10-09 18:49:42 +0200

    contact section added

--- contact.php
+++ contact.php
@@ -1,3 +1,27 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','contact_db');
+
+if(isset($_POST['send'])){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $plan = $_POST['plan'];
+    $address = $_POST['address'];
+    $message = $_POST['message'];
+
+    $insert = "INSERT INTO `contact_form`(`name`, `email`, `number`, `plan`, `address`, `message`) 
+    VALUES ('$name',' $email','$number','$plan',' $address',' $message')";
+
+
+    mysqli_query($conn, $insert);
+
+    header('location:contact.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,6 +49,58 @@
 <div class="container">
 
 <?php @include 'header.php'; ?>
+
+<section class="contact">
+
+    <h1 class="heading">contact us</h1>
+
+    <form action="" method="post">
+
+    <div class="flex">
+
+        <div class="inputBox">
+            <span>your name</span>
+            <input type="text" placeholder="enter your name" name="name" required>
+        </div>
+
+        <div class="inputBox">
+            <span>your email</span>
+            <input type="email" placeholder="enter your email" name="email" required>
+        </div>
+
+        <div class="inputBox">
+            <span>your number</span>
+            <input type="number" placeholder="enter your number" name="number" required>
+        </div>
+
+        <div class="inputBox">
+            <span>choose plan</span>
+            <select name="plan">
+                <option value="basic">basic plan</option>
+                <option value="prem">premium plan</option>
+                <option value="ultimate">ultimate plan</option>
+            </select>
+        </div>
+
+        <div class="inputBox">
+            <span>your address</span>
+            <textarea name="address" placeholder="enter your address" required cols="30" rows="10"></textarea>
+        </div>
+
+        <div class="inputBox">
+            <span>your message</span>
+            <textarea name="message" placeholder="enter your message" required cols="30" rows="10"></textarea>
+        </div>
+
+    </div>
+
+    <input type="submit" value="send message" name="send" class="btn">
+
+
+    </form>
+
+
+</section>
 
 <?php @include 'footer.php'; ?>
 
